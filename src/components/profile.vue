7f2e84898522e56219f706fc8d8b777acb843ba8
ggenee <eugenechoi5@gmail.com>
2017-12-10 22:00:58 -0500

    organizer stuff next
    gotta work on organizer stuff now

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -2,24 +2,41 @@
   <div class="profile">
     <h3>Make Your Profile</h3>
     <h4>Are you a ...?</h4>
-      <input type="radio" id="one" value="Client" v-model="type">
+      <input type="radio" id="one" value="client" v-model="type">
       <label for"one">Client</label>
-      <input type="radio" id="two" value="Organizer" v-model="type">
+      <input type="radio" id="two" value="organizer" v-model="type">
       <label for="two">Organizer</label>
     <button >Connection</button>
   </div>
 </template>
 
+<div>
 <template v-if= "type ==='organizer'">
   <div id = "organizer">
     <input type="text" placeholder="Name" v-model="name"><br>
     <input type="text" placeholder="Twitter" v-model="twitter"><br>
     <input type="text" placeholder="Photo" v-model="photo"><br>
-    <input type="text" placeholder="Photo" v-model="photo"><br>
+    //social interests?
+    //created protests
+    //current protests
+
   </div>
   <button>Connection</button>
 </template>
 
+<template v-if= "type ==='client'">
+  <div id = "organizer">
+    <input type="text" placeholder="Name" v-model="name"><br>
+    <input type="text" placeholder="Phone" v-model="phone"><br>
+    <input type="text" placeholder="Photo" v-model="photo"><br>
+    //social interests
+    //current protest
+    //following protest
+
+  </div>
+  <button>Connection</button>
+</template>
+</div>
 
 
 <script>
@@ -53,7 +70,13 @@ export default {
     });
   },
   createClientData: function(){
-
+  firebase.database.ref('users' + '/' + this.userId).set({
+    name : this.name,
+    type: this.type,
+    phone: this.phone,
+    followingProtests,
+    currentProtests
+  })
   }
 
   }
